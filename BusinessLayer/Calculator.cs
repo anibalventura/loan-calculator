03d1bed040ba425d6e2d71fa5146a4ddf3f67caa
Anibal Ventura <anibalventura20@gmail.com>
2021-11-02 21:09:21 -0400

    Add Calculator logic.

--- BusinessLayer/Calculator.cs
+++ BusinessLayer/Calculator.cs
@@ -4,5 +4,24 @@ namespace BusinessLayer
 {
     public class Calculator
     {
+        public double CalculateLoan(int amount, int type, int months)
+        {
+            double finalInterest = 0;
+
+            switch (type)
+            {
+                case (int)LoanTypeOptions.PERSONAL:
+                    finalInterest = ((double)(int)InterestOptions.PERSONAL / 100) * amount;
+                    break;
+                case (int)LoanTypeOptions.VEHICLE:
+                    finalInterest = ((double)(int)InterestOptions.VEHICLE / 100) * amount;
+                    break;
+                case (int)LoanTypeOptions.MORTAGE:
+                    finalInterest = ((double)(int)InterestOptions.MORTAGE / 100) * amount;
+                    break;
+            }
+
+            return (finalInterest + amount) / months;
+        }
     }
 }
